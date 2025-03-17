8856b6d75d4baef0284b9968795abbf29e884a63
Jimmy32767255 <Jimmy32767255@outlook.com>
2025-03-18 03:33:47 +0800

    Delete Scan-Bluetooth-devices.py

--- Scan-Bluetooth-devices.py
+++
@@ -1,9 +0,0 @@
-import asyncio
-from bleak import discover
-
-async def scan_devices():
-    devices = await discover()
-    for d in devices:
-        print(f"Name: {d.name} | Address: {d.address}")
-
-asyncio.run(scan_devices())
